3249b2896a325e9b81db173af3168fd58e9a3c77
ShamiKantMourya <shamimourya9852@gmail.com>
2024-10-03 19:15:32 +0530

    components render

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-
+import WeatherDetails from "./components/WeatherDetails";
+import WeatherImage from "./components/WeatherImage";
+import SearchBar from "./components/SearchBar";
 
 function App() {
+  // console.log(weatherData, "weather data");
+  // const { main } = weatherData;
+  // console.log(main, "main")
+  // const tempInCelcius = (main.temp - 32) / 1.8;
+  // console.log(tempInCelcius, "temp in c");
   return (
     <div className="App">
-    
+      <main>
+        {/* <WeatherImage /> */}
+        <div className="background-dark"></div>
+        <aside className="sideBar">
+          <SearchBar />
+          <div className="weather">
+            {/* <WeatherDetails /> */}
+          </div>
+        </aside>
+      </main>
     </div>
   );
 }
