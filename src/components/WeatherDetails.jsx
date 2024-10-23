28fcf800f202eb1a51f606152c01855fc68a88b4
ShamiKantMourya <shamimourya9852@gmail.com>
2024-10-23 13:57:57 +0530

    weather detail

--- src/components/WeatherDetails.jsx
+++ src/components/WeatherDetails.jsx
@@ -1,11 +1,21 @@
-import React, {useContext} from 'react'
-import { WeatherContext } from '../context/DataContext';
+import React, { useContext } from "react";
+import { WeatherContext } from "../context/DataContext";
 
 const WeatherDetails = () => {
-  const {weatherData} = useContext(WeatherContext);
+  const { weatherData } = useContext(WeatherContext);
+  const { main, name, weather} = weatherData;
+  const weatherInfo = weather[0];
+  console.log(main,"main")
+  console.log(weatherInfo, "weather info");
+  console.log(weatherData, "weather data");
   return (
-    <div>Weather</div>
-  )
-}
+    <div>
+      <h3>{weatherInfo.description}</h3>
+      <p>
+        Temp max <span>{main.temp_max}</span>
+      </p>
+    </div>
+  );
+};
 
 export default WeatherDetails;
