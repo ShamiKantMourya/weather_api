e95c3cf966f6c0d280684757aa9d939cb554d7aa
ShamiKantMourya <shamimourya9852@gmail.com>
2024-10-23 12:53:33 +0530

    setTimeout

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,17 +1,39 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { FaSearchLocation } from "react-icons/fa";
 
 import { WeatherContext } from "../context/DataContext";
 
 const SearchBar = () => {
-  const { city, setCity, fetchWeatherData } = useContext(WeatherContext);
+  const { city, setCity, fetchWeatherData, setInputValue, inputValue } =
+    useContext(WeatherContext);
+
+  const debounceTime = 500;
+
+  useEffect(() => {
+    // This effect will run when the inputValue changes
+    const timer = setTimeout(() => {
+      // Update city state after the debounce time
+      if (inputValue.trim() !== "") {
+        setCity(inputValue);
+      }
+    }, debounceTime);
+
+    return () => {
+      // Cleanup: Clear the timer on unmount or when inputValue changes
+      clearTimeout(timer);
+    };
+  }, [inputValue]); // Effect depends on inputValue
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value); // Update the inputValue on change
+  };
   return (
     <div className="search_bar">
       <input
         type="text"
         placeholder="Enter city name"
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
+        value={inputValue}
+        onChange={handleChange}
       />
       <FaSearchLocation onClick={fetchWeatherData} />
     </div>
