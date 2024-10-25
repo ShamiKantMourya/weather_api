feadbd455d6b87d72683f1ddf81d37dd734ebffd
ShamiKantMourya <shamimourya9852@gmail.com>
2024-10-25 13:23:03 +0530

    weatherdetails css file added

--- src/components/WeatherDetails.jsx
+++ src/components/WeatherDetails.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { FaTemperatureHalf, FaDroplet } from "react-icons/fa6";
 
 import { WeatherContext } from "../context/DataContext";
+import "../css/weatherDetails.css";
 
 const WeatherDetails = () => {
   const { weatherData } = useContext(WeatherContext);
@@ -12,24 +13,21 @@ const WeatherDetails = () => {
   const weatherInfo = weather[0];
   const minTemp = (main.temp_min -  273.15).toFixed(2);
   const maxTemp = (main.temp_max -  273.15).toFixed(2);
-  // console.log(main,"main")
-  // console.log(weatherInfo, "weather info");
-  // console.log(weatherData, "weather data");
   return (
     <div>
       <h3>{weatherInfo.description}</h3>
       <div>
       <p className="weather_description">
-        Temp max <span className="detail">{maxTemp}°C</span><span className="temp"><FaTemperatureHalf /></span>
+        Temp max <span className="detail">{maxTemp}°C</span><span className="hot_temp"><FaTemperatureHalf /></span>
       </p>
       <p className="weather_description">
-        Temp min <span className="detail">{minTemp}°C</span><span className="temp"><FaTemperatureHalf /></span>
+        Temp min <span className="detail">{minTemp}°C</span><span className="cold_temp"><FaTemperatureHalf /></span>
       </p>
       <p className="weather_description">
         Pressure <span className="detail">{main.pressure}</span>
       </p>
       <p className="weather_description">
-        Humidity <span className="detail">{main.humidity}%</span><span className="temp"><FaDroplet /></span>
+        Humidity <span className="detail">{main.humidity}%</span><span className="humidity"><FaDroplet /></span>
       </p>
       </div>
     </div>
