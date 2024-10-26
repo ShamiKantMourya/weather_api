f1727776ab85199fe8e2eb316cf9adef5321220a
ShamiKantMourya <shamimourya9852@gmail.com>
2024-10-26 16:22:41 +0530

    error message added

--- src/components/WeatherImage.jsx
+++ src/components/WeatherImage.jsx
@@ -1,50 +1,65 @@
-import React,{useContext} from "react";
+import React, { useContext } from "react";
 
-import  useDate from "../hooks/useDate";
+import useDate from "../hooks/useDate";
 import { WeatherContext } from "../context/DataContext";
 import WeatherLogo from "./WeatherLogo";
 import "../css/weatherImage.css";
 
 const WeatherImage = () => {
-    const {weatherData} = useContext(WeatherContext);
-    
-    const dateTime = useDate();
-    // console.log(dateTime.date, dateTime.time,"date time")
+  const { weatherData, errorMessage } = useContext(WeatherContext);
+  console.log(errorMessage, "errorMessage")
 
-    if (!weatherData) {
-      return <div>Loading weather data...</div>;
-    };
+  const dateTime = useDate();
 
-    const { main, name, weather} = weatherData;
-    console.log(main, weather, name, "data received");
-    // const weatherInfo = weather[0]; 
-    const tempInCelcius = (main.temp -  273.15).toFixed(2);
-    // console.log(weatherInfo,"weather")
+  if (!weatherData) {
+    return <div>Loading weather data...</div>;
+  }
+
+  const { main, name, weather } = weatherData;
+  console.log(main, weather, name, "data received");
+  const tempInCelcius = (main.temp - 273.15).toFixed(2);
   return (
-    <div className="imgContainer">
-      <div className="imageBox">
-      {tempInCelcius > 50 ? (
-        <img src={`${process.env.PUBLIC_URL}/summer.jpg`} alt="Summer time" />
-      ) : tempInCelcius >= 15 && tempInCelcius < 50 ? (
-        <img src={`${process.env.PUBLIC_URL}/rain.jpg`} alt="Rainy time" />
+    <>
+      {errorMessage ? (
+        <p>{alert(errorMessage)}</p>
       ) : (
-        <img src={`${process.env.PUBLIC_URL}/winter.jpg`} alt="Winter time" />
+        weatherData && (
+          <div className="imgContainer">
+            <div className="imageBox">
+              {tempInCelcius > 50 ? (
+                <img
+                  src={`${process.env.PUBLIC_URL}/summer.jpg`}
+                  alt="Summer time"
+                />
+              ) : tempInCelcius >= 15 && tempInCelcius < 50 ? (
+                <img
+                  src={`${process.env.PUBLIC_URL}/rain.jpg`}
+                  alt="Rainy time"
+                />
+              ) : (
+                <img
+                  src={`${process.env.PUBLIC_URL}/winter.jpg`}
+                  alt="Winter time"
+                />
+              )}
+            </div>
+            <div className="tempbox-container">
+              <h1 className="temp">{tempInCelcius}°C</h1>
+              <div className="temp-info">
+                <h2 className="temp-location">{name}</h2>
+                <div className="date-time">
+                  <p>{dateTime.date}</p>
+                  <p>{dateTime.time}</p>
+                </div>
+              </div>
+              <div className="weather-logo">
+                <WeatherLogo />
+              </div>
+            </div>
+          </div>
+        )
       )}
-      </div>
-      <div className="tempbox-container">
-      <h1 className="temp">{tempInCelcius}°C</h1>
-      <div className="temp-info">
-        <h2 className="temp-location">{name}</h2>
-        <div className="date-time">
-          <p>{dateTime.date}</p>
-          <p>{dateTime.time}</p>
-        </div>
-      </div>
-      <div className="weather-logo">
-        <WeatherLogo />
-      </div>
-      </div>
-    </div>
+    </>
   );
 };
 
