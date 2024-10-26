15c107454636c1984af812ad42409e341c9ffd85
ShamiKantMourya <shamimourya9852@gmail.com>
2024-10-26 16:20:28 +0530

    error boundary added

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import ReactDOM from "react-dom/client";
 import { WeatherProvider } from "./context/DataContext";
 import "./index.css";
 import App from "./App";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <WeatherProvider>
-      <App />
-    </WeatherProvider>
+    <ErrorBoundary>
+      <WeatherProvider>
+        <App />
+      </WeatherProvider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
