d77e0e02b24426d18428ece943aa6a120e6b482e
ShamiKantMourya <shamimourya9852@gmail.com>
2024-10-26 16:08:21 +0530

    return error

--- src/apiService.js
+++ src/apiService.js
@@ -6,11 +6,12 @@ export const apiCall = async (city) => {
   try {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
     const res = await axios.get(url);
+    // if (!res.ok) throw new Error("City weather location not available");
     if (res.status === 200) {
       return res.data;
     }
   } catch (error) {
-    return error.message;
+    return error;
   }
 };
 
